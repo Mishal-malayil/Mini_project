b92ea3c43dca5db1f577c22b78a68ca6f32acf82
Mishal-malayil <mishalsonu2003@gmail.com>
2026-07-29 18:16:22 +0530

    Event categories

--- frontend/College-event-management-admin/src/app/core/services/event-category.ts
+++ frontend/College-event-management-admin/src/app/core/services/event-category.ts
@@ -1,6 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class EventCategory {}
+export class EventCategoryService {
+
+  private apiUrl = environment.apiUrl + '/event-categories';
+
+  constructor(private http: HttpClient) {}
+
+  // Get All Categories
+  getCategories(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  // Get Single Category
+  getCategory(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  // Add Category
+  addCategory(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  // Update Category
+  updateCategory(id: number, data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+
+  // Delete Category
+  deleteCategory(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+}
--- frontend/College-event-management-admin/src/app/pages/event-categories/event-categories.ts
+++ frontend/College-event-management-admin/src/app/pages/event-categories/event-categories.ts
@@ -1,10 +1,202 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { EventCategoryService } from '../../core/services/event-category';
 
 @Component({
   selector: 'app-event-categories',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './event-categories.html',
-  styleUrl: './event-categories.css',
+  styleUrl: './event-categories.css'
 })
-export class EventCategories {}
+export class EventCategories implements OnInit {
+
+  categories: any[] = [];
+
+  category = {
+    category_name: '',
+    description: '',
+    status: 'active'
+  };
+
+  selectedCategory: any = {};
+
+  editCategoryData: any = {
+    id: '',
+    category_name: '',
+    description: '',
+    status: 'active'
+  };
+
+  constructor(
+    private categoryService: EventCategoryService
+  ) {}
+
+  ngOnInit(): void {
+
+    this.loadCategories();
+
+  }
+
+  // Load Categories
+
+  loadCategories() {
+
+    this.categoryService.getCategories().subscribe({
+
+      next: (res: any) => {
+
+        this.categories = res;
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+      }
+
+    });
+
+  }
+
+  // Add Category
+
+  addCategory(form: NgForm) {
+
+    if (form.invalid) {
+
+      form.control.markAllAsTouched();
+
+      return;
+
+    }
+
+    this.categoryService.addCategory(this.category).subscribe({
+
+      next: () => {
+
+        alert("Category Added Successfully");
+
+        this.loadCategories();
+
+        form.resetForm();
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+      }
+
+    });
+
+  }
+
+  // View Category
+
+  viewCategory(id: number) {
+
+    this.categoryService.getCategory(id).subscribe({
+
+      next: (res: any) => {
+
+        this.selectedCategory = res;
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+      }
+
+    });
+
+  }
+
+  // Edit Category
+
+  editCategory(id: number) {
+
+    this.categoryService.getCategory(id).subscribe({
+
+      next: (res: any) => {
+
+        this.editCategoryData = { ...res };
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+      }
+
+    });
+
+  }
+
+  // Update Category
+
+  updateCategory() {
+
+    this.categoryService.updateCategory(
+
+      this.editCategoryData.id,
+      this.editCategoryData
+
+    ).subscribe({
+
+      next: () => {
+
+        alert("Category Updated Successfully");
+
+        this.loadCategories();
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+      }
+
+    });
+
+  }
+
+  // Delete Category
+
+  deleteCategory(id: number) {
+
+    if (confirm("Delete this Category?")) {
+
+      this.categoryService.deleteCategory(id).subscribe({
+
+        next: () => {
+
+          alert("Category Deleted");
+
+          this.loadCategories();
+
+        },
+
+        error: (err) => {
+
+          console.log(err);
+
+        }
+
+      });
+
+    }
+
+  }
+
+}
