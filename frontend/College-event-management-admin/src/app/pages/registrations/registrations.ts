dc809ce008d7a95a913ab680fce14508250d713e
Mishal-malayil <mishalsonu2003@gmail.com>
2026-08-03 13:01:45 +0530

    Registrations

--- frontend/College-event-management-admin/src/app/pages/registrations/registrations.ts
+++ frontend/College-event-management-admin/src/app/pages/registrations/registrations.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrationService } from '../../core/services/registration';
 
 @Component({
   selector: 'app-registrations',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './registrations.html',
-  styleUrl: './registrations.css',
+  styleUrl: './registrations.css'
 })
-export class Registrations {}
+export class Registrations implements OnInit {
+
+  registrations: any[] = [];
+
+  selectedRegistration: any = {};
+
+  constructor(
+    private registrationService: RegistrationService
+  ) { }
+
+  ngOnInit(): void {
+
+    this.loadRegistrations();
+
+  }
+
+  // Load All Registrations
+
+  loadRegistrations() {
+
+    this.registrationService.getRegistrations().subscribe({
+
+      next: (res: any) => {
+
+        this.registrations = res;
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+      }
+
+    });
+
+  }
+
+  // View Registration
+
+  viewRegistration(id: number) {
+
+    this.registrationService.getRegistration(id).subscribe({
+
+      next: (res: any) => {
+
+        this.selectedRegistration = res;
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+      }
+
+    });
+
+  }
+
+}
